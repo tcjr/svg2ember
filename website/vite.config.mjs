775d86b8e3d8aeb1967eb45ff14387b07b6af276
Tommy Carter <tommyjr@gmail.com>
2025-06-16 15:46:28 -0500

    Updated plugin to respect typescript flag when generating virtual id

--- website/vite.config.mjs
+++ website/vite.config.mjs
@@ -8,10 +8,9 @@ import Inspect from 'vite-plugin-inspect';
 export default defineConfig({
   plugins: [
     Inspect(),
-    svg2ember({ optimize: true, typescript: true }),
+    svg2ember(),
     classicEmberSupport(),
     ember(),
-    // extra plugins here
     babel({
       babelHelpers: 'runtime',
       extensions,
